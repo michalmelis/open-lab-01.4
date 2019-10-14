e0eacf189827c52f67f53f00c1ff836aee8249c1
michalmelis <michalmelis.sk@gmail.com>
2019-10-14 08:46:00 +0200

    open-lab-01.4

---
+++ open-lab-01.4/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace open_lab_01._4
+{
+    class Program
+    {
+        public static double TriArea(double height, double foundation)
+        {
+            return (height * foundation / 2);
+        }
+        
+    }
+}
